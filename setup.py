c377f0b41d30f1eba8348c5bfba4224c36791788
stuchly <grigorij@seznam.cz>
2020-03-13 13:27:27 +0000

    everything changes here - prepared for ivis implementation

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='vaevictis',
-      version='0.1.1',
+      version='0.2.0',
       description='test',
       packages=['vaevictis'],
       zip_safe=False)
