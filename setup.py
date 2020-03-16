dcf310993e74d34f31ead658854e31c10f938eb3
stuchly <grigorij@seznam.cz>
2020-03-16 17:24:22 +0000

    require numba

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(name='vaevictis',
       version='0.2.1',
       description='test',
-      install_requires=["annoy"],
+      install_requires=["annoy","numba"],
       packages=['vaevictis'],
       zip_safe=False)
