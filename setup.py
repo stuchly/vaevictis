ec26e3b59cade8470bc70b381941caab4ea55ed1
stuchly <grigorij@seznam.cz>
2020-03-16 22:12:19 +0000

    require all

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup
 setup(name='vaevictis',
       version='0.2.1',
       description='test',
-      install_requires=["annoy","numba","tensorflow","tqdm"],
+      install_requires=["annoy","numba",
+      "tensorflow","tqdm","scipy","multiprocessing",
+      "collections","operator","time","numpy",
+      "random","sys", "os", "json"],
       packages=['vaevictis'],
       zip_safe=False)
