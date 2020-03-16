d40aeff1a6a150377b947f88fad34cd862bbdc6d
stuchly <grigorij@seznam.cz>
2020-03-16 17:46:00 +0000

    require tensorflow

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(name='vaevictis',
       version='0.2.1',
       description='test',
-      install_requires=["annoy","numba"],
+      install_requires=["annoy","numba","tensorflow"],
       packages=['vaevictis'],
       zip_safe=False)
