f7b14295327575cf8b350392fe1afbb232c515fb
stuchly <grigorij@seznam.cz>
2020-03-16 22:02:31 +0000

    require tqdm

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(name='vaevictis',
       version='0.2.1',
       description='test',
-      install_requires=["annoy","numba","tensorflow"],
+      install_requires=["annoy","numba","tensorflow","tqdm"],
       packages=['vaevictis'],
       zip_safe=False)
